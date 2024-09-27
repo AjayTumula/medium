d2aec79dce9b1f2f1e5fbc42d3f33b7496105a9b
AjayTumula <ajaytumula72@gmail.com>
2024-09-27 17:36:28 +0530

    add frontend of the application

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,19 @@
+
+
+export const Quote = () => {
+    return <div className="h-screen bg-slate-200 flex flex-col justify-center">
+        <div className="flex justify-center">
+            <div className="max-w-lg">
+                <div className="text-3xl font-bold">
+                    "The customer I received was exceptional. The support team went above and beyond to address my concerns."
+                </div>
+                <div className="max-w-md text-xl font-semibold mt-2">
+                    Julies Winfield
+                </div>
+                <div className="max-w-md text-sm font-light text-slate-400">
+                    CEO | Acme corp
+                </div>
+            </div>  
+        </div>
+    </div>
+}
